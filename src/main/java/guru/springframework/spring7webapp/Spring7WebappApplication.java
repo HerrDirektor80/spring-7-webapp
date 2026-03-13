7bee6f74cc5d23c0665421312da77607e3e03e68
Christian Wenzel <christian@superwenzel.de>
2026-03-13 12:30:13 +0100

    Initial commit after start.spring.io

--- src/main/java/guru/springframework/spring7webapp/Spring7WebappApplication.java
+++ src/main/java/guru/springframework/spring7webapp/Spring7WebappApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring7WebappApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Spring7WebappApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7WebappApplication.class, args);
+	}
 
 }
